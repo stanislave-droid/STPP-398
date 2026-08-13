7d58bbd62cf1feda439d0950922adf6f0ee058ff
Stanislave <stasdroner@gmail.com>
2026-08-13 17:25:00 +0300

    Add animation changing depending on the width of the screen

--- src/js/header-links-observer.js
+++ src/js/header-links-observer.js
@@ -2,6 +2,12 @@ import AOS from 'aos';
 import 'aos/dist/aos.css';
 const sections = document.querySelectorAll('[data-observer="section"]');
 const links = document.querySelectorAll('[data-link]');
+const firstReviewCard = document.querySelector(
+  '[data-review-animation="first"]'
+);
+const secondReviewCard = document.querySelector(
+  '[data-review-animation="second"]'
+);
 
 const observerProperties = {
   root: null,
@@ -24,6 +30,26 @@ const observer = new IntersectionObserver(entries => {
 }, observerProperties);
 
 sections.forEach(section => observer.observe(section));
+
+window.innerWidth >= 1440
+  ? setReviewCardsAnimation(1440)
+  : setReviewCardsAnimation(375);
+
+function setReviewCardsAnimation(width) {
+  if (width >= 1440) {
+    firstReviewCard.dataset.aos = 'fade-up';
+    secondReviewCard.dataset.aos = 'fade-left';
+  } else {
+    firstReviewCard.dataset.aos = 'fade-left';
+    secondReviewCard.dataset.aos = 'fade-right';
+  }
+}
+
+window.addEventListener('resize', event => {
+  const width = event.currentTarget.innerWidth;
+  setReviewCardsAnimation(width);
+  AOS.refresh();
+});
 
 AOS.init({
   duration: 1000,
